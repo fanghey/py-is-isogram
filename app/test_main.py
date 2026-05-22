12e96c48b4c38ff10b3ed17da3c5b99b76ff3988
fanghey <2603Lysenko2603@gmail.com>
2026-05-22 16:00:05 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,12 @@
-from app.main import is_isogram
+import pytest
+from main import is_isogram  
 
-# write your code here
+
+def test_is_isogram_returns_true_for_word_without_repeating_letters():
+    assert is_isogram('playgrounds') is True
+def test_is_isogram_returns_false_for_word_with_repeating_letters():
+    assert is_isogram('look') is False
+def test_is_isogram_is_case_insensitive():
+    assert is_isogram('Adam') is False
+def test_is_isogram_returns_true_for_empty_string():
+    assert is_isogram('') is True
